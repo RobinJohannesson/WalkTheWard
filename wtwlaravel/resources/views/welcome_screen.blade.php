87b22ab9306cd86011b8beef482e13bd42a54722
Dailyman <robin.lars.hakansson@gmail.com>
2017-10-30 12:32:14 +0100

    Fix index.php route

--- wtwlaravel/resources/views/welcome_screen.blade.php
+++ wtwlaravel/resources/views/welcome_screen.blade.php
@@ -8,7 +8,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="{{url('/')}}/css/style.css">
+
 </head>
 <body>
     <div class="container-fluid">
@@ -27,7 +28,7 @@
                 <h2 class="text-center">Vandring i vården - Ett aktivitetsspel :D</h2>
             </div>
             <div class="col-md">
-                <a href="index.php/registration">col-sm</a>
+                <a href="{{url('/')}}/registration">col-sm</a>
             </div>
 
         </div>
