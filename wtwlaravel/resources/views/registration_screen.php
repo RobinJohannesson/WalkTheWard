fb15e9f78abf004a0bf07043a77fdde9f94ad23f
RobinJohannesson <robinjohannesson96@gmail.com>
2017-10-30 11:31:29 +0100

    lagt till welcome screen till laravel som route

---
+++ wtwlaravel/resources/views/registration_screen.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="../public/css/style.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row justify-content-end">
+            <div class="col col-md-3">
+                <img src="./images/help.png" alt="Help">
+                <p class="text-center">Hjälp</p>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md">
+                <img src="./images/logo.png" alt="Logo">
+            </div>
+            <div class="col-md-6">
+                <h1 class="text-center">Walk the Ward</h1>
+                <h2 class="text-center">Vandring i vården - Ett aktivitetsspel :D</h2>
+            </div>
+            <div class="col-md">
+                registrering
+            </div>
+
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-3">
+                <div class="btn-new-user">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+</html>
--- wtwlaravel/routes/web.php
+++ wtwlaravel/routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome_screen');
+});
+
+Route::get('/registration', function () {
+    return view('registration_screen');
 });
