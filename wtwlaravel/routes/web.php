8cb12ef20666f996c14031988439891f31358707
RobinJohannesson <robinjohannesson96@gmail.com>
2017-10-30 13:20:06 +0100

    alla routes

--- wtwlaravel/routes/web.php
+++ wtwlaravel/routes/web.php
@@ -18,3 +18,35 @@ Route::get('/', function () {
 Route::get('/registration', function () {
     return view('registration_screen');
 });
+
+Route::get('/information', function () {
+    return view('information_screen');
+});
+
+Route::get('/map', function () {
+    return view('map_screen');
+});
+
+Route::get('/theme', function () {
+    return view('theme_screen');
+});
+
+Route::get('/scan', function () {
+    return view('scan_screen');
+});
+
+Route::get('/question', function () {
+    return view('question_screen');
+});
+
+Route::get('/bonus', function () {
+    return view('bonus_screen');
+});
+
+Route::get('/home', function () {
+    return view('home_screen');
+});
+
+Route::get('/feedback', function () {
+    return view('feedback_screen');
+});
