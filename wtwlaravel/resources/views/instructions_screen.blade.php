6c0a75412cc03793e354753f67d5aeb12740e707
ChristinaTsigara <christina.tsigara@gmail.com>
2017-11-15 14:51:20 +0100

    Instructions 2nd screen
    Instruktioner om hur man spelar wtw

---
+++ wtwlaravel/resources/views/instructions_screen.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{url('/')}}/css/style.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row justify-content-end">
+            <div class="col col-md-3">
+                <img src="./images/help.png" alt="Help">
+                <p class="text-center">Hjälp</p>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md">
+            
+                <h1 class="text-center">Syftet av spelet</h1>
+                
+            </div>
+        
+
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                
+             <p>För att starta ett spel börjar du med att välja en del av Skåne du vill vandra i. Skåne är indelat i 5 olika delar där du går från stad till stad inom det valda området. Efter det väljer du ett av de teman som finns som du vill svara frågor på.. Sedan kommer du till en sida där du använder dig av kameran från surfplattan eller telefonen och scannar en QR-kod för att få fram en fråga.</p>
+             <p>Du får fyra svarsalternativ till varje fråga. Svarar du inte rätt den första gången får du försöka tills du väljer rätt alternativ av svaren.Börja med att leta upp en station. Stationen innehåller en QR kod som du scannar genom att hålla upp kameran med din surfplatta eller telefon. Koderna finns uppsatta i korridoren på avdelningen. Du får spela när du vill och hur länge du vill under tiden som du ligger inlagd.</p>
+            
+            </div>
+        </div>
+        
+        <div class="row justify-content-end">
+            <div class="col col-md-2">
+                <button type="button" class="btn btn-primary btn-lg">Fortsätt</button>
+        
+             </div>
+        </div>
+    
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+</html>
