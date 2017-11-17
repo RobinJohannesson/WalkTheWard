e6052bcee63ef5269911521783869014e83315a8
ChristinaTsigara <christina.tsigara@gmail.com>
2017-11-17 13:17:35 +0100

    added font style Amatic SC

--- wtwlaravel/resources/views/information_screen.blade.php
+++ wtwlaravel/resources/views/information_screen.blade.php
@@ -9,6 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="{{url('/')}}/css/style.css">
+     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
 </head>
 <body>
     <div class="container-fluid">
