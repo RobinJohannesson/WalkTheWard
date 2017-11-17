7a4d7aece8260872ca09448af411948678bf67c4
ChristinaTsigara <christina.tsigara@gmail.com>
2017-11-17 14:54:57 +0100

    welcome screen

--- wtwlaravel/resources/views/welcome_screen.blade.php
+++ wtwlaravel/resources/views/welcome_screen.blade.php
@@ -16,28 +16,28 @@
 </head>
 <body>
     <div class="container-fluid">
-        <div class="row justify-content-end">
+        <div class="row justify-content-center">
+            <div class="col-md">
+                <img src="./images/logo.png" alt="Logo" class="img-responsive center-block">
+            </div>
             <div class="col col-md-3">
                 <img src="./images/help.png" alt="Help">
                 <p class="text-center">Hjälp</p>
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-md">
-                <img src="./images/logo.png" alt="Logo" class="img-responsive center-block">
-            </div>
+            
             <div class="col-md-6">
                  <h1 class="text-center">Walk The Ward </h1>
                 <h2 class="text-center">Vandring i vården - Ett aktivitetsspel </h2>
             </div>
-            <div class="col-md">
-                <a href="{{url('/')}}/registration">col-sm</a>
-            </div>
+
 
         </div>
         <div class="row justify-content-center">
             <div class="col-md-3">
                 <div class="btn-new-user">
+                     <a href="{{url('/')}}/registration">create new user</a>
                 </div>
             </div>
         </div>
