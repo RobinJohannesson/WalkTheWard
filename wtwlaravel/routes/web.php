d61781b5b2b70cf9cbb66a302e00f7c0b4718ea8
RobinJohannesson <robinjohannesson96@gmail.com>
2017-11-17 13:48:00 +0100

    start controller

--- wtwlaravel/routes/web.php
+++ wtwlaravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,51 +13,64 @@
 */
 
 // Route::group([], function () {
-  // if (dd(Cookie::get('patientId') == null)
-  // if (true)
-  // {
-  //
-  //   return redirect()->route("registration");
-  //
-  // }
-  // vvv Add Routes Under This vvv
+// if (dd(Cookie::get('patientId') == null)
+// if (true)
+// {
+//
+//   return redirect()->route("registration");
+//
+// }
+// vvv Add Routes Under This vvv
+//Route::any('/{all}', function ($all) {
+    //dd($all);
+    //$allCont = $all . "Controller";
+    //$allCont = ucfirst($allCont);
+    // if (dd(Cookie::get('patientId')) != null) {
+    //Route::GET($all, $allCont);
+    //Route::GET('', 'WelcomeController');
+        //$url = secure_url($all);
+        //return redirect()->route($all);
+    // }
+//});
+if($request->hasCookie('patientId')) {
+    // do something
+}
 
+Route::resource('', "WelcomeController");
 
-  Route::resource('', "WelcomeController");
+// Route::get('get-cookie', function() {
+//   dd(Cookie::get('patientId')); // showing you different ways to set / get the cookie
+// });
 
-  // Route::get('get-cookie', function() {
-  //   dd(Cookie::get('patientId')); // showing you different ways to set / get the cookie
-  // });
+Route::GET('registration', ['as' => 'registration', 'uses' => "RegistrationController@index"]);
 
-  Route::GET('registration', ['as' => 'registration', 'uses' => "RegistrationController@index"]);
+Route::POST('registration', "RegistrationController@store");
 
-  Route::POST('registration', "RegistrationController@store");
+Route::resource('information', "InformationController");
 
-  Route::resource('information', "InformationController");
+Route::resource('instructions', "InstructionsController");
 
-  Route::resource('instructions', "InstructionsController");
+Route::resource('map', "MapController");
 
-  Route::resource('map', "MapController");
+Route::resource('theme', "ThemeController");
 
-  Route::resource('theme', "ThemeController");
+Route::resource('scan', "ScanController");
 
-  Route::resource('scan', "ScanController");
+Route::resource('question', "QuestionController");
 
-  Route::resource('question', "QuestionController");
+Route::resource('question/{id}', 'QuestionController');
 
-  Route::resource('question/{id}', 'QuestionController');
+Route::resource('bonus', "BonusController");
 
-  Route::resource('bonus', "BonusController");
+Route::resource('home', "HomeController");
 
-  Route::resource('home', "HomeController");
+Route::resource('feedback', "FeedbackController");
 
-  Route::resource('feedback', "FeedbackController");
+Route::resource('statistics', "StatisticsController");
 
-  Route::resource('statistics', "StatisticsController");
+Auth::routes();
 
-  Auth::routes();
-
-  Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 
 
