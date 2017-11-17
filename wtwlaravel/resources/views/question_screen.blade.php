e8365bf452e9cb5c21c384dcabe97c8d069c1e29
ChristinaTsigara <christina.tsigara@gmail.com>
2017-11-17 10:36:02 +0100

    question

--- wtwlaravel/resources/views/question_screen.blade.php
+++ wtwlaravel/resources/views/question_screen.blade.php
@@ -5,11 +5,12 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
-
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="{{url('/')}}/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+
 </head>
 <body>
     <div class="container-fluid">
