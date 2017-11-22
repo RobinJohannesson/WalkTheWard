753f41d6ea97b35425515afe47df3f2d58b3116b
RobinJohannesson <robinjohannesson96@gmail.com>
2017-11-22 16:57:07 +0100

    tema

--- wtwlaravel/app/Http/Controllers/ThemeController.php
+++ wtwlaravel/app/Http/Controllers/ThemeController.php
@@ -3,84 +3,117 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\patient;
+use App\game;
+use App\place;
+use App\place_in_game;
+use App\area;
 use App\theme;
+use App\question;
+use App\question_in_game;
 
 class ThemeController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function index()
     {
         //
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function create()
     {
         //
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
     public function store(Request $request)
     {
-        $Game = Game::where("id", "=", $request->patientId)->first();
-        $Game->currentTheme = $request->themeSelected;
-        $Game->save();
+        try {
+            $themeId = $request->themeId;
+            $gameId = $request->gameId;
+            Game::where(['id' => $gameId])->update(['themeId' => $themeId]);
+        }
+        catch (\Exception $e) {
+            $error = $e->getMessage();
+            $response = array(
+                'status' => 'error',
+                'msg' => $error
+            );
+            return response()->json($response);
+        }
+
+        $response = array(
+            'status' => 'success'
+        );
+
+        return response()->json($response);
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show()
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function show(Request $request)
     {
+        // Hämta PatientId från cookie
+        $patientId = $request->cookie('patientId');
+
+        // Hämta CurrentAreaId
+        $patient = Patient::find($patientId);
+
+        // Hämta GameId
+        $gameId = $patient->game->id;
+
         $theme = Theme::all();
-        return view('theme_screen', compact("theme"));
+
+        return view('theme_screen', compact(["theme", "gameId"]));
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function edit($id)
     {
         //
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function update(Request $request, $id)
     {
         //
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function destroy($id)
     {
         //
