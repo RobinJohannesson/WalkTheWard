5bdb906b487ce342ab5038f1ed77a5a5fe7ab522
RobinJohannesson <robinjohannesson96@gmail.com>
2017-11-22 17:19:21 +0100

    map

--- wtwlaravel/resources/views/map_screen.blade.php
+++ wtwlaravel/resources/views/map_screen.blade.php
@@ -54,10 +54,16 @@
 
     </div>
     <!-- Optional JavaScript -->
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <script src="{{url('/')}}/js/jquery.rwdImageMaps.min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function(e) {
+        $('img[usemap]').rwdImageMaps();
+    });
+    </script>
 </body>
 </html>
