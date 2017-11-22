b8efb04aa84324106ab3c09ee055f503e9a39bc2
Ann-Helen <annhelen.nilsson89@gmail.com>
2017-11-22 18:09:43 +0100

    Informationskarusell info sida

--- wtwlaravel/resources/views/information_screen.blade.php
+++ wtwlaravel/resources/views/information_screen.blade.php
@@ -8,12 +8,15 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet">
     <link rel="stylesheet" href="/css/font-awesome.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="{{url('/')}}/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
 </head>
 <body>
+  <div class="container">
+
     <div class="container-fluid">
         <div class="row justify-content-end">
             <div class="col col-md-3">
@@ -26,28 +29,49 @@
             <div class="col-md">
                   <h1 class="text-center"><strong>Syftet med spelet</strong></h1>
              </div>
-        
-     <div class="row justify-content-center">
-            <div class="col-md-8">
 
-             <p>Spelet heter ”Walk the Ward” eller på svenska ”Vandring i vården”. Spelet går ut på att du går runt i Skåne och på vägen besvarar frågor med olika tema som du själv väljer. Syftet med spelet är att du rör på dig under din tid i vården. Det är viktigt för ditt tillfrisknande att du inte ligger stilla på sängen under din vistelse på sjukhuset. Utan att du är aktiv, rör på dig efter förmåga och medverkar i din vård.
-             </p>
+            <div id="info-carousel" class="carousel slide" data-ride="carousel" data-interval="false">
 
+            <!-- Your controls -->
+            <ol class="carousel-indicators">
+              <li data-target="#info-carousel" data-slide-to="0" class="active"></li>
+              <li data-target="#info-carousel" data-slide-to="1"></li>
+              <li data-target="#info-carousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Your slides -->
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <p>Spelet heter ”Walk the Ward” eller på svenska ”Vandring i vården”. Spelet går ut på att du går runt i Skåne och på vägen besvarar frågor med olika tema som du själv väljer. Syftet med spelet är att du rör på dig under din tid i vården. Det är viktigt för ditt tillfrisknande att du inte ligger stilla på sängen under din vistelse på sjukhuset. Utan att du är aktiv, rör på dig efter förmåga och medverkar i din vård.
+                </p>
+              </div>
+              <div class="carousel-item">
+                <p>För att starta en spelomgång börjar du med att välja en del av Skåne du vill vandra i. Skåne är indelat i 5 olika delar där du går från stad till stad inom det valda området. Efter detta väljer du ett av de teman som finns som du vill svara frågor på. Därefter kommer du till en sida där du använder dig av kameran från surfplattan eller telefonen och skannar en QR-kod för att få fram en fråga.</p>
+              </div>
+              <div class="carousel-item">
+                <p>Du får fyra svarsalternativ till varje fråga. Svarar du inte rätt den första gången får du försöka tills du väljer rätt alternativ av svaren. Börja med att leta upp en station. Stationen innehåller en QR-kod som du skannar genom att hålla upp kameran med din surfplatta eller telefon. Koderna finns uppsatta i korridoren på avdelningen. Du får spela när du vill och hur länge du vill under tiden som du ligger inlagd.</p>
+                <p>Behöver du hjälp under spelets gång, tryck på det blåa frågetecknet uppe i högra hörnet för mer instruktioner.</p>
+              </div>
             </div>
-        </div>
+            <a class="carousel-control-prev" href="#info-carousel" role="button" data-slide="prev">
+              <!--<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span> -->
+              <button type="button" class="button continue_button">Tillbaka</button>
+            </a>
+            <a class="carousel-control-next" href="#info-carousel" role="button" data-slide="next">
+              <!--<span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span> -->
+              <button type="button" class="button continue_button">Fortsätt</button>
+            </a>
+          </div>
 
-        <div class="row justify-content-end">
-            <div class="col col-md-4">
-                <button type="button" class="button continue_button">Fortsätt</button>
-            </div>
-        </div>
-
-    </div>
+</div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="js/animations.js"></script>
 </body>
 </html>
