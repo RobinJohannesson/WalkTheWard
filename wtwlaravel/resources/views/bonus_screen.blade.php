cc088e5ae4da1e55b59f9790fcdc59831d63f37f
Robin Håkansson <Dailyman@users.noreply.github.com>
2017-11-23 22:25:07 +0100

    New stuff to bonus page

--- wtwlaravel/resources/views/bonus_screen.blade.php
+++ wtwlaravel/resources/views/bonus_screen.blade.php
@@ -8,42 +8,139 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{url('/')}}/css/font-awesome.css">
     <link rel="stylesheet" href="{{url('/')}}/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+    <link rel="stylesheet" href="{{url('/')}}/css/style-bonus.css">
 </head>
 <body>
     <div class="container-fluid">
         <div class="row justify-content-end">
-            <div class="col col-md-3">
-                <img src="./images/help.png" alt="Help">
-                <p class="text-center">Hjälp</p>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md">
-                <img src="./images/logo.png" alt="Logo">
-            </div>
-            <div class="col-md-6">
-                <h1 class="text-center">Walk the Ward</h1>
-                <h2 class="text-center">Vandring i vården - Ett aktivitetsspel :D</h2>
-            </div>
-            <div class="col-md">
-                bonus
-            </div>
-
-        </div>
-        <div class="row justify-content-center">
             <div class="col-md-3">
-                <div class="btn-new-user">
-
+                <div class="text-right">
+                    <i class="fa fa-question-circle fa_custom fa-3x" aria-hidden="true" style="position:static;"></i>
                 </div>
             </div>
+        </div>
+
+        <div class="row justify-content-center">
+
+            <div class="col">
+                <div class="text-center puzzle">
+                    <div class="row no-gutters puzzle-row justify-content-around">
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row no-gutters puzzle-row justify-content-around">
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row no-gutters puzzle-row justify-content-around">
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row no-gutters puzzle-row justify-content-around">
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                        <div class="col-3 puzzle-piece">
+                            <div>
+                                <p>M</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <h1 id="bonus-word" class="text-center">
+                    <span class="letter-m">_</span><span class="letter-a">_</span><span class="letter-l">_</span><span class="letter-m">_</span><span class="letter-ö">Ö</span>
+                </h1>
+            </div>
+
         </div>
     </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+    $(document).ready(function(e) {
+        $('.puzzle-piece').on('click', function(event) {
+            event.preventDefault();
+            $(this).addClass('hidden-piece');
+            $(this).children("div").children("p").hide();
+        });
+    });
+    </script>
 </body>
 </html>
