2572aba5e2a6dd785914684dbbab013ec50739d9
RobinJohannesson <robinjohannesson96@gmail.com>
2017-11-23 16:58:08 +0100

    tog bort id

--- wtwlaravel/app/Http/Controllers/StatisticsController.php
+++ wtwlaravel/app/Http/Controllers/StatisticsController.php
@@ -60,7 +60,7 @@ class StatisticsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show()
     {
         return view("statistics_screen");
     }
