e0285a833e706e2a79c3c73e236f7027e4ac13a7
RobinJohannesson <robinjohannesson96@gmail.com>
2017-11-24 14:44:48 +0100

    statistics visa/dölj förklara varför

--- wtwlaravel/resources/views/statistics_screen.blade.php
+++ wtwlaravel/resources/views/statistics_screen.blade.php
@@ -27,14 +27,14 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-md-6"> 
+            <div class="col-md-6">
                 <h1 class="text-center">Statistik</h1>
             </div>
 
         </div>
         <div class="row justify-content-center">
             <div class="col-md-3">
-                    <form action="{{{ url("statistics/store") }}}" method="POST">
+                    <form action="{{{ url("statistics/store") }}}" id="statisticsForm" method="POST">
                         {{ csrf_field() }}
                         <p>Ska du gå hem?</p>
                         <label class="radio-inline"><input type="radio" name="hasGoneHome" value="1"> Ja<br></label>
@@ -48,15 +48,15 @@
                             <option value="5">5</option>
                         </select>
                         <p>Var spelet enkelt att spela?</p>
-                        <label class="radio-inline"><input type="radio" name="wasEasyToPlay" value="1"> Ja<br></label>
+                        <label class="radio-inline"><input id="wasEasyToPlayRadio" type="radio" name="wasEasyToPlay" value="1"> Ja<br></label>
                         <label class="radio-inline"><input type="radio" name="wasEasyToPlay" value="0"> Nej<br></label>
                         <div class="form-group row">
-                            <label for="text-input" class="col-form-label">Förklara varför:</label>
-                            <input class="form-control" type="text" id="text-input">
+                            <label for="text-input" class="col-form-label statisticsWhy">Förklara varför:</label>
+                            <input class="form-control statisticsWhy" type="text" id="text-input">
                         </div>
                         <input type="submit" id="submit_button" value="Skicka">
                       </form>
-             
+
             </div>
         </div>
     </div>
@@ -66,5 +66,17 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    $('.statisticsWhy').hide();
+    $(document).ready(function(){
+        $('#statisticsForm').change(function() {
+        if ($('#wasEasyToPlayRadio').prop('checked')) {
+            $('.statisticsWhy').show();
+        } else {
+            $('.statisticsWhy').hide();
+        }
+        });
+    });
+    </script>
 </body>
 </html>
