2c585204cbf0f3851a651de278f91f44f470bf58
RobinJohannesson <robinjohannesson96@gmail.com>
2017-12-07 11:12:29 +0100

    scanningen justerar camera beroende på enhet

--- wtwlaravel/resources/views/scan.blade.php
+++ wtwlaravel/resources/views/scan.blade.php
@@ -84,9 +84,13 @@
             window.location.href = content;
         });
         Instascan.Camera.getCameras().then(function (cameras) {
-            if (cameras.length > 0) {
+            if (cameras.length > 1) {
+                scanner.start(cameras[1]);
+            }
+            if (cameras.length <= 1) {
                 scanner.start(cameras[0]);
-            } else {
+            }
+            else {
                 console.error('No cameras found.');
             }
         }).catch(function (e) {
